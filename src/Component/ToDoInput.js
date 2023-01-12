0a4510abbaf36c42d756aaf549bb4c2e3e6f4692
Manohar2107 <manoharkum99@gmail.com>
2023-01-12 13:37:30 +0530

    input task can be added by enter click too

--- src/Component/ToDoInput.js
+++ src/Component/ToDoInput.js
@@ -12,7 +12,8 @@ class ToDoInput extends Component{
         this.handleInput=this.handleInput.bind(this);
     }
 
-    handleAddTask(){
+    handleAddTask(evt){
+        evt.preventDefault();
         this.props.onAddingTask(this.state.todo);
         this.setState(
             {todo:""}
@@ -31,14 +32,14 @@ class ToDoInput extends Component{
         return(
             <article style={{padding:"20px"}}>
                 <h2>To-Do Input</h2>
-                <div className="container" style={{border:"1px solid lightcyan",alignItems:"center",width:"70%",margin:"auto",padding:"20px"}}>
+                <form onSubmit={this.handleAddTask} className="container" style={{border:"1px solid lightcyan",alignItems:"center",width:"70%",margin:"auto",padding:"20px"}}>
                     <div className="row" style={{marginBottom:"10px"}}>
                         <div className="col-xs-12 col-12">
                             <span  style={{backgroundColor:"cyan",padding:"5px"}} className="fa fa-book"></span>
                             <input type="text" name="todo" value={this.state.todo} onChange={this.handleInput} placeholder="New todo" className="col-xs-11 col-11"></input></div>
                         </div>
-                        <Button type="submit" className="col-xs-12 col-12" onClick={this.handleAddTask}>New Task</Button>
-                </div>
+                        <Button type="submit" className="col-xs-12 col-12">New Task</Button>
+                </form>
             </article>
         );
     }
